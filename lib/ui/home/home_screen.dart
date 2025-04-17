3b67ee1653379768037b50870217dfcde9c841e8
sringrid95 <sringrid95@gmail.com>
2025-04-17 20:47:24 +0000

    home widgets

---
+++ lib/ui/home/home_screen.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      drawer: Drawer(),
+      appBar: AppBar(
+        actions: [IconButton(onPressed: () {}, icon: Icon(Icons.login))],
+      ),
+      body: Padding(
+        padding: EdgeInserts.symmetric(horizontal: 24.0),
+        child: Column(
+          spacing: 32.0,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Center(child: Image.asset('assets/logo.png', width: 147)),
+            Text("Boas-Vindas!"),
+            TextFormField(),
+            Text("Escolha por categoria"),
+            Image.asset("assets/banners/banner_promo.png"),
+            Text("Bem avaliados"),
+          ],
+        ),
+      ),
+    );
+  }
+}
